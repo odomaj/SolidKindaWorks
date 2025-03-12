4259b893e3c7c140d3331acfd0931f0f7a7cb6d1
odomaj <143726318+odomaj@users.noreply.github.com>
2025-03-12 07:56:04 -0400

    main display resize w/ screen

--- src/app.py
+++ src/app.py
@@ -1,47 +1,77 @@
 from pathlib import Path
 
 from PySide6.QtWidgets import QMainWindow, QApplication, QLabel
-from PySide6.QtCore import QFile
+from PySide6.QtCore import QFile, QSize, QPoint
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtGui import QImage, QPixmap, QImageReader
+from PySide6.QtGui import QImage, QPixmap, QImageReader, QResizeEvent
 import sys
 from views.view import Viewer
+from enum import Enum
+import common_types
 import numpy as np
 
-QT_UI_DIR: Path = QFile(Path(__file__).parent.joinpath("qt"))
-
-
-def load_ui():
-    ui_file = QFile(Path(__file__).parent.joinpath("qt").joinpath("main.ui"))
-    if ui_file.open(QFile.ReadOnly):
-        loader = QUiLoader()
-        window = loader.load(ui_file)
-        ui_file.close()
-        return window
+QT_UI_DIR: Path = Path(__file__).parent.joinpath("qt")
+UI_FILE: str = "main.ui"
 
 
 class MainWindow(QMainWindow):
+    class WindowState(Enum):
+        FILE = 1
+        HOME = 2
+        INSERT = 3
+        VIEW = 4
+
+    viewer: Viewer = Viewer()
+    state: WindowState = WindowState.FILE
+
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.ui_dim_percentage = 1
-        self.home_widget = load_ui()
 
-        width = 256
-        height = 256
-        random_pixels = np.random.randint(
-            0, 255, size=(width, height, 3), dtype=np.uint8
-        )
-        self.q_image = QImage(
-            random_pixels.data, width, height, 3 * width, QImage.Format_RGB888
-        )
-
-        image_label = self.home_widget.findChild(QLabel, "label")
-        self.pixMap = QPixmap.fromImage(self.q_image)
-        image_label.setPixmap(self.pixMap)
+        self.load_ui()
+        self.display: QLabel = self.home_widget.findChild(QLabel, "display")
 
         self.resize(800, 600)
         self.setCentralWidget(self.home_widget)
+
         self.show()
+
+    def load_ui(self) -> None:
+        ui_file = QFile(QT_UI_DIR.joinpath(UI_FILE))
+        if ui_file.open(QFile.ReadOnly):
+            loader = QUiLoader()
+            self.home_widget = loader.load(ui_file)
+            ui_file.close()
+
+    def update_display(self) -> None:
+        dimensions: common_types.Display = common_types.Display(
+            width=np.int32(self.display.size().width()),
+            height=np.int32(self.display.size().height()),
+        )
+
+        raster: common_types.Raster = self.viewer.render(dimensions, None)
+
+        image: QImage = QImage(
+            raster.data,
+            dimensions.width,
+            dimensions.height,
+            3 * dimensions.width,
+            QImage.Format_RGB888,
+        )
+
+        self.display.setPixmap(QPixmap.fromImage(image))
+
+    def resize_display(self) -> None:
+        screen_size: QSize = self.size()
+        display_pos: QPoint = self.display.pos()
+
+        self.display.resize(
+            screen_size.width() - display_pos.x(),
+            screen_size.height() - display_pos.y(),
+        )
+
+    def resizeEvent(self, event: QResizeEvent) -> None:
+        self.resize_display()
+        self.update_display()
 
 
 def main() -> None:
--- src/views/view.py
+++ src/views/view.py
@@ -1,5 +1,7 @@
 from views import view_types, ray_trace, standard
 
+import numpy as np
+
 
 class Viewer:
     view_mode: view_types.Perspective = view_types.Perspective.ORTHOGONAL
@@ -10,6 +12,9 @@ class Viewer:
         display: view_types.common_types.Display,
         figures: list[view_types.common_types.Display],
     ) -> view_types.common_types.Raster:
+        return np.random.randint(
+            0, 256, size=(display.width, display.height, 3), dtype=np.uint8
+        )
         if self.light_mode == view_types.Lighting.STANDARD:
             return standard.render(display, figures)
         if self.light_mode == view_types.Lighting.RAY_TRACE:
