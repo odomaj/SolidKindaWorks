3cbb84b4415bed5971d67d7688fc6a35c702f815
Megan <edwardmm@Clarkson.edu>
2025-03-27 17:12:19 -0400

    stops showing plotter

--- src/views/rasterize.py
+++ src/views/rasterize.py
@@ -9,7 +9,7 @@ def render(
     meshes: Meshes,
     cam: camera.Camera,
 ) -> view_types.Raster:
-    plotter = vedo.Plotter(offscreen=False)
+    plotter = vedo.Plotter(offscreen=True)
     for key in meshes.meshes:
         plotter.add(meshes.meshes[key])
 
